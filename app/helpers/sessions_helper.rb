11f70352395cfb69149cf67a9f9fe938edf87aa6
Anidhya Ahuja <anidhya@weboniselab.com>
2011-08-03 19:45:12 +0530

    successfully signed -in and out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,15 +1,19 @@
 module SessionsHelper
 
 
-  def current_user
-   @current_user = @current_user || user_from_remember_token
-  end
+
 
   def sign_in(user)
     cookies.permanent.signed[:remember_token] = [user.id, user.salt]
-    #self.current_user = user
+    self.current_user = user
   end
 
+  def current_user=(user)
+    @current_user = user
+  end
+  def current_user
+   @current_user ||= user_from_remember_token
+  end
 
 
   def signed_in?
